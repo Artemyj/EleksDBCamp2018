90cd76fc3f6f9a9c2bdd201c26ced3b0781fa6b5
Artemii Melnyk <rr608643@fincoad.com>
2018-11-11 18:01:34 +0200

    Add test and scripts

---
+++ Lection8/Lection8/Test/Test.sql
@@ -0,0 +1,16 @@
+﻿    
+    EXEC tSQLt.NewTestClass 'testData';
+GO
+
+CREATE PROCEDURE testData.[test that Products do not have Weight with negative Weight]
+AS
+BEGIN
+
+    DECLARE @actual decimal(8,2);
+   
+    SET @actual = (Select TOP 1 Weight  From AdventureWorksLT2012.SalesLT.Product Where Weight<=0)
+
+    EXEC tSQLt.AssertEquals @Expected=NULL, @Actual=@actual 
+
+END;
+GO
